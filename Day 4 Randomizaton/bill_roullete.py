d4584fc19904fe89df35d2aa74b565429e094fb8
zubaircodes-bit <zubairali.code@gmail.com>
2025-10-04 19:52:32 +0500

    add index of max-people using len function

--- Day 4 Randomizaton/bill_roullete.py
+++ Day 4 Randomizaton/bill_roullete.py
@@ -1,4 +1,8 @@
-names_string = input("Enter Your Names Separated by Commas.")
+import random
+names_string = input("Enter Your Names Separated by Commas : ")
 names = [name.strip() for name in names_string.split(",")]
-print(names_string)
-print(names)
+# print(names_string)
+# print(names)
+names_len = len(names)
+random_person = random.randint(0, names_len - 1)
+print(f"{names[random_person]} is going to pay the bill!")
